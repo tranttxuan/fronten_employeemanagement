0ee28476d724eccd1da9fee88bc21ed765537020
xuan.tranthithanh <xuan.tranthithanh@thalesdigital.io>
2022-05-03 23:48:32 +0200

    add method to delete employee from database

--- src/app/components/dropdown/dropdown.component.ts
+++ src/app/components/dropdown/dropdown.component.ts
@@ -17,6 +17,7 @@ export class DropdownComponent implements OnInit {
   @Input() public handleDropdown: (param: number | undefined) => void;
 
   @Output()  openModalEvent = new EventEmitter<IOpenModalEvent> ();
+  @Output() deleteEmployeeEvent = new EventEmitter<Employee>();
 
   public deleteModalType = ModalType.DELETE_EMPLOYEE;
   public editModalType = ModalType.EDIT_EMPLOYEE;
@@ -29,6 +30,10 @@ export class DropdownComponent implements OnInit {
 
   onOpenModal (employee: Employee | undefined, idModal: ModalType){
     this.openModalEvent.emit({employee: employee, id:idModal});
+  }
+
+  deleteEmployee(employee: Employee | undefined){
+    this.deleteEmployeeEvent.emit(employee);
   }
 }
 
